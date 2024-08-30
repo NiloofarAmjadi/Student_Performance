b14c6276c875c296514ab6cd58f3129c7f72d3c6
Niloofar <amjadi_nilooofar@yahoo.com>
2024-08-29 23:26:31 -0700

    first commit

---
+++ app.py
@@ -0,0 +1,39 @@
+from flask import Flask, request, jsonify,render_template
+import pandas as pd
+import pickle
+
+# Create a Flask app
+app = Flask(__name__)
+filename = 'svm_student_performance_model.pkl'
+model = pickle.load(open(filename, 'rb'))    # load the model
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+
+@app.route('/predict', methods=['POST'])
+def predict():
+    # Collect data from form submission
+    math_score = request.form['math']
+    reading_score = request.form['reading']
+    writing_score = request.form['writing']
+
+    # Create a DataFrame
+    data = {
+        'math score': [math_score],
+        'reading score': [reading_score],
+        'writing score': [writing_score]
+    }
+    df = pd.DataFrame(data)
+
+    # Make prediction
+    prediction = model.predict(df)
+
+    # Return the result
+    return render_template('index.html', predict=prediction[0])
+
+
+# Run the app
+if __name__ == "__main__":
+    app.run(debug=True)
